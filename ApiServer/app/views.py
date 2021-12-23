70df6c20dafca371b03194050e22ca91990d8680
kino-n1851 <kino.n1851@gmail.com>
2021-12-23 05:58:32 +0000

    YOLOでのモザイク処理への対応

--- ApiServer/app/views.py
+++ ApiServer/app/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets, filters
 from .models import User, Entry
 from .serializer import UserSerializer, EntrySerializer
 from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
 import json
 import tensorflow as tf
@@ -13,6 +14,14 @@ from PIL import Image, ImageOps
 import numpy as np
 import base64
 from io import BytesIO
+import sys
+sys.path.append("/home/keino/YOLOv3")
+from keras_yolo3 import *
+
+yolo_args = {   "model_path": '/home/keino/YOLOv3/keras_yolo3/model_data/yolo.h5',
+        "anchors_path": '/home/keino/YOLOv3/keras_yolo3/model_data/yolo_anchors.txt',
+        "classes_path": '/home/keino/YOLOv3/keras_yolo3/model_data/coco_classes.txt'}
+yolo = YOLO_mosaic()
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -40,4 +49,29 @@ def mnist(request):
         s = model.predict_classes( img.reshape([1,28,28]) )
         print(s)
         return Response(s[0])
+    return Response(-1)
+
+@api_view(['GET', 'POST'])
+def  mosaic_YOLO(request):
+    global yolo
+    if request.method == 'POST':
+        data = request.data
+        json_load = json.loads(data)
+        img_str = json_load["img"]
+
+        img = base64.b64decode(img_str)
+        img = BytesIO(img)
+        img = Image.open(img)
+        img.save("received_YOLO.png")
+        r_img = yolo.detect_image(img)
+        r_img.save("convert_YOLO.png")
+        byte = BytesIO()
+        r_img.save(byte, format="png")
+        img_byte = byte.getvalue()
+        img_base64 = base64.b64encode(img_byte)
+        img_str = img_base64.decode("utf-8")
+        ret = {
+            "img":img_str
+        }   
+        return JsonResponse(ret)
     return Response(-1)
