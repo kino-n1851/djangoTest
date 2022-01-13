5598bc19da65fa877ed760f8e747dd9ef2f350e8
kino-n1851 <kino.n1851@gmail.com>
2022-01-13 17:44:51 +0900

    work:定期処理機能の追加

--- mainServer/app/apps.py
+++ mainServer/app/apps.py
@@ -5,3 +5,11 @@ class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
     verbose_name = "sample app"
+
+    def ready(self):
+        """
+        This function is called when startup.
+        """
+        print("updating start.")
+        from .search import start
+        start()
---
+++ mainServer/app/search.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+from io import BytesIO
+from ftplib import FTP
+import base64
+import os
+import requests
+import json
+from PIL import Image
+
+def update():
+    ftp = FTP("153.127.69.176", "iijimalab00", passwd="island#00")
+    items = ftp.nlst(".")
+    print(items)
+    for item in items:
+        if not item.endswith(".png") or item.endswith("_mosaic.png"):
+            continue
+        filename = item
+        print(filename)
+        byte = BytesIO()
+        ftp.retrbinary('RETR '+ filename, byte.write)
+        byte.seek(0)
+        img_byte = byte.getvalue()
+        img_base64 = base64.b64encode(img_byte)
+        img_str = img_base64.decode("utf-8")
+        req_img = {
+            "img":img_str
+        }   
+        r = requests.post("http://172.16.20.170:8000/api/mosaic/",json=json.dumps(req_img))
+        #print(r.json())
+        if True:
+            if not 'json' in r.headers.get('content-type'):
+                continue
+            else:
+                json_load = r.json()
+                img_str = json_load["img"]
+                if img_str is None:
+                    print("response is broken.")
+                    return
+                img = base64.b64decode(img_str)
+                img = BytesIO(img)
+                img = Image.open(img)
+                img.save("updated.png")
+                byte = BytesIO()
+                img.save(byte, format="png")
+                byte.seek(0)
+                ftp.delete(filename)
+                ftp.storbinary('STOR '+ os.path.splitext(filename)[0] + "_mosaic.png", byte)
+
+    #except Exception as e:
+    #    print(e)
+
+def printt():
+    print("updated!!")
+
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(update, 'interval', hours=1) # schedule
+    scheduler.start()
+
+if __name__ == "__main__":
+    update()
